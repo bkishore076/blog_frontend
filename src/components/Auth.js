36db9c2fe218bb745a69ea326da0fb74f2471399
bkishore <bkishore076@gmail.com>
2022-12-15 08:10:43 +0530

    second

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
 
 const Auth = () => {
+  const [isSignup, setisSignup] = useState(false)
   return (
-    <div>Auth</div>
-  )
-}
+    <div>
+      <form>
+        <Box
+        maxWidth={400}
+          display="flex"
+          flexDirection={"coloumn"}
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin='auto'
+          marginTop={5}
+          borderRadius={5}
+        >
+          <Typography variant="h2" padding={3} textAlign='center'>Login</Typography>
+          <TextField margin="normal" placeholder="Name" />
+          <TextField type={'email'} margin="normal" placeholder="Email" />
+          <TextField type={'password'}margin="normal" placeholder="Password" />
+          <Button variant="contained"color="warning" sx={{borderRadius:3,marginTop:3}}>Submit</Button>
+          <Button sx={{borderRadius:3}}>Change to Signup</Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
 
-export default Auth
+export default Auth;
