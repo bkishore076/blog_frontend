26593fe6f2aa691c30c5134658cd2fee06299e02
bkishore <bkishore076@gmail.com>
2022-12-20 18:42:01 +0530

    fifth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,28 +3,73 @@ import { Box } from "@mui/system";
 import React from "react";
 
 const Auth = () => {
-  const [isSignup, setisSignup] = useState(false)
+  const [isSignup, setisSignup] = useState(false);
+  const handleChange = (e)=>{
+    setInputs(prevState)
+  }
+  const [inputs, setinputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
   return (
     <div>
       <form>
         <Box
-        maxWidth={400}
+          maxWidth={400}
           display="flex"
           flexDirection={"coloumn"}
           alignItems="center"
           justifyContent={"center"}
           boxShadow="10px 10px 20px #ccc"
           padding={3}
-          margin='auto'
+          margin="auto"
           marginTop={5}
           borderRadius={5}
         >
-          <Typography variant="h2" padding={3} textAlign='center'>Login</Typography>
-          <TextField margin="normal" placeholder="Name" />
-          <TextField type={'email'} margin="normal" placeholder="Email" />
-          <TextField type={'password'}margin="normal" placeholder="Password" />
-          <Button variant="contained"color="warning" sx={{borderRadius:3,marginTop:3}}>Submit</Button>
-          <Button sx={{borderRadius:3}}>Change to Signup</Button>
+          <Typography variant="h2" padding={3} textAlign="center">
+            Login
+          </Typography>
+          {isSignup ? "Signup" : "Login"}
+          {isSignup && (
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              margin="normal"
+              placeholder="Name"
+            />
+          )}
+          {""}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            margin="normal"
+            placeholder="Email"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            margin="normal"
+            placeholder="Password"
+          />
+          <Button
+            variant="contained"
+            color="warning"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setisSignup(!isSignup)}
+            sx={{ borderRadius: 3 }}
+          >
+            Change to Signup{isSignup ? "Login" : "Signup"}
+          </Button>
         </Box>
       </form>
     </div>
